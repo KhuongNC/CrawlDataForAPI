c819ef7ff793c4badd364938ef5844f792ce434e
KhuongNC <anhkhuongmurom@gmail.com>
2022-06-28 16:41:23 +0700

    Add DI

--- CrawlDataForAPI/App_Start/WebApiConfig.cs
+++ CrawlDataForAPI/App_Start/WebApiConfig.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Unity;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using CrawlDataForAPI.Repositories;
+using CrawlDataForAPI.Models;
 
 namespace CrawlDataForAPI
 {
@@ -11,13 +11,17 @@ namespace CrawlDataForAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var container = new UnityContainer();
+            container.RegisterType<IMovieRepository, MovieRepository>();
+            container.RegisterType<INewsRepository, NewsRepository>();
+            config.DependencyResolver = new UnityResolver(container);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
 
