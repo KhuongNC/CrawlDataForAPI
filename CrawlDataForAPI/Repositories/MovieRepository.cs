0f993c6af2d2e3701e47f5452392eec1c9ab8b0e
KhuongNC <anhkhuongmurom@gmail.com>
2022-06-28 16:10:30 +0700

    Add Repositories folder

---
+++ CrawlDataForAPI/Repositories/MovieRepository.cs
@@ -0,0 +1,25 @@
+﻿using CrawlDataForAPI.Models;
+using CrawlDataForAPI.Utilities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CrawlDataForAPI.Repositories
+{
+    public interface IMovieRepository
+    {
+        List<Movie> GetAllMovies();
+        List<Movie> GetMovieByName(string name);
+    }
+    public class MovieRepository : IMovieRepository
+    {
+        public List<Movie> GetMovieByName(string name)
+        {
+            return CrawlData.CrawlDataFromWebsite<Movie>(WebsiteLink.BHDSTAR, Constants.BHDSTAR).Where(m => m.Name.ToLower().Contains(name)).ToList();
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            return CrawlData.CrawlDataFromWebsite<Movie>(WebsiteLink.BHDSTAR, Constants.BHDSTAR);
+        }
+    }
+}
---
+++ CrawlDataForAPI/Repositories/NewsRepository.cs
@@ -0,0 +1,25 @@
+﻿using CrawlDataForAPI.Models;
+using CrawlDataForAPI.Utilities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CrawlDataForAPI.Repositories
+{
+    public interface INewsRepository
+    {
+        List<News> GetAllNews();
+        List<News> GetNewsByTitle(string title);
+    }
+    public class NewsRepository : INewsRepository
+    {
+        public List<News> GetNewsByTitle(string title)
+        {
+            return CrawlData.CrawlDataFromWebsite<News>(WebsiteLink.KENH14, Constants.KENH14).Where(m => m.Title.ToLower().Contains(title)).ToList();
+        }
+
+        public List<News> GetAllNews()
+        {
+            return CrawlData.CrawlDataFromWebsite<News>(WebsiteLink.KENH14, Constants.KENH14);
+        }
+    }
+}
