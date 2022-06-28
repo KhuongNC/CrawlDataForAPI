3d5387fa2b6735e4b556fc9c3030d0092feb70c1
KhuongNC <anhkhuongmurom@gmail.com>
2022-06-28 16:17:07 +0700

    Create Movies and News controller

---
+++ CrawlDataForAPI/Controllers/MoviesController.cs
@@ -0,0 +1,60 @@
+﻿using CrawlDataForAPI.Repositories;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CrawlDataForAPI.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        private IMovieRepository _movieRepository;
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        /// <summary>
+        /// Get movies (12) from website https://www.bhdstar.vn/
+        /// </summary>
+        /// <returns></returns>
+        public HttpResponseMessage GetMoviesFromBhdStar()
+        {
+            try
+            {
+                var movies = _movieRepository.GetAllMovies();
+                return Request.CreateResponse(HttpStatusCode.OK, movies);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Get movies by name from website https://www.bhdstar.vn/
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public HttpResponseMessage GetMoviesByNameFromBhdStar(string name)
+        {
+            try
+            {
+                var movies = _movieRepository.GetMovieByName(name);
+
+                if (movies == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "Movie with name " + name + " not found");
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, movies);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.ToString());
+            }
+        }
+
+    }
+
+}
---
+++ CrawlDataForAPI/Controllers/NewsController.cs
@@ -0,0 +1,61 @@
+﻿using CrawlDataForAPI.Repositories;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CrawlDataForAPI.Controllers
+{
+    public class NewsController : ApiController
+    {
+        private INewsRepository _newsRepository;
+        public NewsController(INewsRepository newsRepository)
+        {
+            _newsRepository = newsRepository;
+        }
+
+        /// <summary>
+        /// Get news (30) from website https://kenh14.vn/
+        /// </summary>
+        /// <returns></returns>
+        public HttpResponseMessage GetAllNewsFromKenh14()
+        {
+            try
+            {
+                var newsList = _newsRepository.GetAllNews();
+                return Request.CreateResponse(HttpStatusCode.OK, newsList);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// <summary>
+        /// Get movie by name from website https://kenh14.vn/
+        /// </summary>
+        /// <param name="title"></param>
+        /// <returns></returns>
+        public HttpResponseMessage GetNewsFromKenh14ByTitle(string title)
+        {
+            try
+            {
+                var news = _newsRepository.GetNewsByTitle(title);
+
+                if (news.Count() == 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "News with name " + title + " not found");
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, news);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.ToString());
+            }
+        }
+
+    }
+}
