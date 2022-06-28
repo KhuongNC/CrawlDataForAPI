ec2da88e2ce4fc216a162883098ce7d2c5658073
KhuongNC <anhkhuongmurom@gmail.com>
2022-06-28 15:29:10 +0700

    Add Movie.cs and News.cs to Models folder

---
+++ CrawlDataForAPI/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrawlDataForAPI.Models
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+        public string Director { get; set; }
+        public string Actors { get; set; }
+        public string TypeOfMovie { get; set; }
+        public string PremiereDate { get; set; }
+        public string Duration { get; set; }
+        public string Language { get; set; }
+        public string Rated { get; set; }
+        public string Content { get; set; }
+        public string ImageLink { get; set; }
+        public string TrailerLink { get; set; }
+    }
+}
---
+++ CrawlDataForAPI/Models/News.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrawlDataForAPI.Models
+{
+    public class News
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ImageUrl { get; set; }
+        public string TypeOfNews { get; set; }
+        public string PostDate { get; set; }
+    }
+}
